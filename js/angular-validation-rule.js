0da0d1ba38e75fa403fb8dd96970f8aa472f0eaa
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-08-23 14:14:46 -0700

    verification working

--- js/angular-validation-rule.js
+++ js/angular-validation-rule.js
@@ -1,39 +1,51 @@
+'use strict';
+
 (function() {
-    angular.module('validation.rule', ['validation'])
-        .config(['$validationProvider',
-            function($validationProvider) {
+  angular.module('validation.rule', ['validation'])
+    .config(['$validationProvider',
 
-                var expression = {
-                    required: function(value) {
-                        return !!value;
-                    },
-                    url: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
-                    email: /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
-                    number: /^\d+$/
-                };
 
-                var defaultMsg = {
-                    required: {
-                        error: 'This should be Required!!',
-                        success: 'It\'s Required'
-                    },
-                    url: {
-                        error: 'This should be Url',
-                        success: 'It\'s Url'
-                    },
-                    email: {
-                        error: 'This should be Email',
-                        success: 'It\'s Email'
-                    },
-                    number: {
-                        error: 'This should be Number',
-                        success: 'It\'s Number'
-                    }
-                };
 
-                $validationProvider.setExpression(expression).setDefaultMsg(defaultMsg);
+      function($validationProvider) {
 
-            }
-        ]);
+        var expression = {
+          required: function(value) {
+            return !!value;
+          },
+          ccv: /^\d{3}$/,
+          credit: /^\d{16}$/,
+          url: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+          email: /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+          number: /^\d{}$/,
+          phone: /^\d{10}$/,
+          zip: /^\d{5}$/
+        };
+
+        var defaultMsg = {
+          ccv: {
+            error: 'Must have exactly 3 digits'
+          },
+          credit: {
+            error: 'Must have exactly 16 digits'
+          },
+          required: {
+            error: 'This field is required'
+          },
+          phone: {
+            error: 'Must have exactly 10 digits'
+          },
+          zip: {
+            error: 'Must have exacly 5 digits'
+          }
+        };
+
+        $validationProvider.setExpression(expression).setDefaultMsg(defaultMsg);
+        $validationProvider.showSuccessMessage = false;
+        $validationProvider.setErrorHTML(function(msg) {
+          return "<span class=\"error\"> " + msg + "</span>";
+        });
+
+      }
+    ]);
 
 }).call(this);
--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
- angular.module('checkoutForm', ['validation'])
-    .controller('CheckoutController', ['$scope', function($scope) {
-      $scope.master = {};
+'use strict';
 
-      $scope.update = function(user) {
-        $scope.master = angular.copy(user);
+angular.module('checkoutForm', ['validation', 'validation.rule'])
+  .controller('CheckoutController', ['$scope',
+    function($scope) {
+
+      $scope.checkout = function() {
+        console.log($scope.checkoutForm.first);
       };
-
-      $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-      };
-
-      $scope.reset();
-
-      $scope.buyNow = function() {
-        console.log($scope.user);
-        console.log("buy now function running in js/main.js");
-      }
-    }]);
+    }
+  ]);
