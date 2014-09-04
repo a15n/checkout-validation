712dd19ff35ddad88ff0631a83d32a7315e199a1
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-09-03 18:01:02 -0700

    deployed to heroku

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 'use strict';
 
-// web.js
 var express = require('express');
-var logfmt = require('logfmt');
 var app = express();
 
-// make the files public
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/img', express.static(__dirname + '/img'));
-app.use('/js', express.static(__dirname + '/js'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname));
 
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/', function(request, response) {
+  response.sendfile('index.html');
 });
 
-app.get('*', function(req, res){
-  res.sendfile('404.html');
+app.get('*', function(request, response){
+	response.send('404: page unavailable');
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log('Listening on ' + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
